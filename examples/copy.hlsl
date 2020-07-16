6d6532979b8f00298ed0cbc3633c5b0218b64cd4
Marijn Suijten <marijn@traverseresearch.nl>
2020-07-16 21:07:11 +0200

    Test shader for OpPhi

--- examples/copy.hlsl
+++ examples/copy.hlsl
@@ -4,5 +4,11 @@ RWTexture2D<float4> g_output : register(u0, space0);
 [numthreads(8, 8, 1)]
 void copyCs(uint3 dispatchThreadId : SV_DispatchThreadID)
 {
-	g_output[dispatchThreadId.xy] = g_input[dispatchThreadId.xy];
+	int x = 0;
+	if (dispatchThreadId.x == 0) {
+		x = g_output[dispatchThreadId.xz] = g_output[dispatchThreadId.yz];
+	} else {
+		x = g_output[dispatchThreadId.yz] = g_output[dispatchThreadId.xz];
+	}
+	g_output[dispatchThreadId.xy] = g_input[dispatchThreadId.xy] + x;
 }
